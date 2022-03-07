d895088715f962e736b6a4c0264aa4c28678ef76
zeekim <41632082+CryZJ@users.noreply.github.com>
2022-03-08 00:07:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: '!',
-  author: 'Zijian Yu',
+  author: 'Yu',
   email: 'ohmygod@email.cn',
   link: 'https://spacegrey.top',
   description: 'This gonna be an awesome website.',
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://weibo.com/u/7328113462',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
