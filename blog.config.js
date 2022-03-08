428e26b1b6b44fececeb936b95ea8f3f0da8180b
zeekim <41632082+CryZJ@users.noreply.github.com>
2022-03-08 22:29:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,8 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app/**Hello**.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg&widths=auto', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://weibo.com/u/7328113462',
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app/**Hello**.png',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
