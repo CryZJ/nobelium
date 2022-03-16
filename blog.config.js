85ca347bfb7684736f7e5e77e383c3cb02b01556
zeekim <41632082+CryZJ@users.noreply.github.com>
2022-03-16 13:33:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -6,12 +6,12 @@ const BLOG = {
   description: 'showing wonders in life.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'serif', // ['sans-serif', 'serif']
+  font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: '', // If leave this empty, current year will be used.
-  postsPerPage: 10,
+  postsPerPage: 15,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
